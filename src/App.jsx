d9c917cbaa40ba8ca74e5003f6bbadaeb8fdc89d
jonytellodev <jonathantello.dev@gmail.com>
2023-07-22 00:13:38 -0300

    se inicia React y se instala Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>estado inicial</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
