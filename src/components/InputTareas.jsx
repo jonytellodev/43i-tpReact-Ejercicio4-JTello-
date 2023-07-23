dd40bf297435343a1cf2bb56840fcee7a867beed
jonytellodev <jonathantello.dev@gmail.com>
2023-07-23 20:15:52 -0300

    se agrega logica para capturar texto de tareas nuevas en input

--- src/components/InputTareas.jsx
+++ src/components/InputTareas.jsx
@@ -1,20 +1,43 @@
 import { Form, Button, FormGroup, FormControl } from "react-bootstrap";
 import "../App.css";
 import ListadoTareas from "./ListadoTareas";
+import { useState } from "react";
+
+
 
 const InputTareas = () => {
+
+  const [capturaTarea,setCapturaTarea] = useState('');
+  const [agregarTarea, setAgregarTarea] = useState([]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setAgregarTarea([...agregarTarea,capturaTarea]);
+    setCapturaTarea('');
+  }
+
   return (
     <>
-      <Form className="d-flex justify-content-center">
-        <Form.Group
+      <Form className="d-flex justify-content-center" onSubmit={handleSubmit}>
+        <FormGroup
           className="mb-2 mt-5 inputAndListWidth d-flex"
           controlId="exampleForm.ControlInput1"
         >
-          <Form.Control type="text" placeholder="Tarea #" className="me-3" />
-          <Button type="submit" variant="primary">
+          <FormControl
+          type="text"
+          placeholder="Tarea #"
+          className="me-3"
+          value={capturaTarea}
+          onChange={(e)=>setCapturaTarea(e.target.value)}
+
+          />
+          <Button
+          type="submit"
+          variant="primary"
+          >
             Agregar
           </Button>
-        </Form.Group>
+        </FormGroup>
       </Form>
       <ListadoTareas></ListadoTareas>
     </>
